2ee17525dbdc8689366733f8705cad6806492f4d
bxm426 <h12345678>
2021-01-21 10:55:19 +0800

    提供http 路由的方法

--- transport/http.go
+++ transport/http.go
@@ -2,8 +2,15 @@ package transport
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
-	"log"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/transport"
+	kithttp "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
+
+	endpts "micro-go/endpoint"
 	"net/http"
 )
 
@@ -15,7 +22,38 @@ var (
 	ErrorBadRequest = errors.New("invalid request parameter")
 )
 
-func MakeHttpHandler(ctx context.Context, logger log.Logger) http.Handler {
+func MakeHttpHandler(ctx context.Context, endpoints endpts.DiscoveryEndpoints, logger log.Logger) http.Handler {
+	r := mux.NewRouter()
+
+	// 定义处理处理器
+	options := []kithttp.ServerOption{
+		kithttp.ServerErrorHandler(transport.NewLogErrorHandler(logger)),
+		kithttp.ServerErrorEncoder(encodeError),
+	}
+
+	r.Methods("GET").Path("/say-hello").Handler(kithttp.NewServer(
+		endpoints.SayHelloEndpoint, decodeSayHelloRequest))
 
 	return nil
+}
+
+// 编码请求参数为 SayHelloRequest
+func decodeSayHelloRequest(_ context.Context, r *http.Request) (interface{}, error) {
+
+}
+
+// 解码 response 结构体为http JSON 响应
+func encodeJsonResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+	return json.NewEncoder(w).Encode(response)
+}
+
+// 解码业务逻辑中出现的err 到 http响应
+func encodeError(ctx context.Context, err error, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+	switch err {
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	json.NewEncoder(w).Encode(map[string]interface{}{"error": err.Error()})
 }
