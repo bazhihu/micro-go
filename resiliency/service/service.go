c704314ebd08122575ce6dc492c9c79fb25f9582
bxm426 <h12345678>
2021-02-02 02:17:00 +0800

    string-service上级服务

--- resiliency/service/service.go
+++ resiliency/service/service.go
@@ -1,1 +1,19 @@
 package service
+
+import (
+	"micro-go/common/discover"
+	"micro-go/common/loadbalance"
+)
+
+// Service constants
+const (
+	StringServiceCommandName = "String.string"
+	StringService            = "string"
+)
+
+type UseStringService struct {
+	// 服务发现客户端
+	discoveryClient discover.DiscoveryClient
+	// 负载均衡器
+	loadbalance loadbalance.LoadBalance
+}
