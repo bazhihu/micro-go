f550bd3a5b60d07a40e2d71a7282568a4a932eda
bxm426 <h12345678>
2021-02-02 11:07:16 +0800

    service

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module micro-go
 go 1.13
 
 require (
+	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5 // indirect
 	github.com/armon/go-metrics v0.0.0-20190430140413-ec5e00d3c878 // indirect
 	github.com/coreos/bbolt v1.3.3 // indirect
 	github.com/coreos/etcd v3.3.25+incompatible
--- resiliency/service/service.go
+++ resiliency/service/service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"errors"
+	"github.com/afex/hystrix-go/hystrix"
 	"micro-go/common/discover"
 	"micro-go/common/loadbalance"
 )
@@ -11,9 +13,27 @@ const (
 	StringService            = "string"
 )
 
+var (
+	ErrHystrixFallbackExecute = errors.New("hystrix fall back execute")
+)
+
+type Service interface {
+	// 远程调用 string-service 服务
+}
+
 type UseStringService struct {
 	// 服务发现客户端
 	discoveryClient discover.DiscoveryClient
 	// 负载均衡器
 	loadbalance loadbalance.LoadBalance
+}
+
+func NewUseStringService(client discover.DiscoveryClient, lb loadbalance.LoadBalance) UseStringService {
+	hystrix.ConfigureCommand(StringServiceCommandName, hystrix.CommandConfig{
+		RequestVolumeThreshold: 5,
+	})
+	return &UseStringService{
+		discoveryClient: nil,
+		loadbalance:     nil,
+	}
 }
