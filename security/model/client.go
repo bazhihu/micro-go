122cd2a5012dcb5960a2c0233b45fc1526524fbb
bxm426 <h12345678>
2021-02-04 15:33:43 +0800

    统一认证的model 信息

--- security/model/client.go
+++ security/model/client.go
@@ -1,1 +1,17 @@
 package model
+
+// 客户端信息
+type ClientDetails struct {
+	// client的标示
+	ClientId string
+	// client的密钥
+	ClientSecret string
+	// 访问令牌有效时间 秒
+	AccessTokenValiditySeconds int
+	// 刷新令牌有效时间 秒
+	RefreshTokenValiditySeconds int
+	// 重定向地址 授权码类型中使用
+	RegisteredRedirectUri string
+	// 可以使用的授权类型
+	AuthorizedGrantTypes []string
+}
--- security/model/user.go
+++ security/model/user.go
@@ -1,1 +1,13 @@
 package model
+
+// 用户信息
+type UserDetails struct {
+	// 用户标示
+	UserId int
+	// 用户名 唯一
+	Username string
+	// 用户密码
+	Password string
+	// 用户具有的权限
+	Authorities []string
+}
