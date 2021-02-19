744f1035457d821c2c3bd3b00c7450acc532e186
bxm426 <h123456>
2021-02-20 01:40:50 +0800

    加段注释

--- security/service/token_service.go
+++ security/service/token_service.go
@@ -133,23 +133,28 @@ type DefaultTokenService struct {
 	tokenEnhancer TokenEnhancer
 }
 
-func (DefaultTokenService) GetOAuth2DetailsByAccessToken(tokenValue string) (*model.OAuth2Details, error) {
+// 根据访问令牌获取对应的用户信息和客户端信息
+func (tokenService *DefaultTokenService) GetOAuth2DetailsByAccessToken(tokenValue string) (*model.OAuth2Details, error) {
 	panic("implement me")
 }
 
-func (DefaultTokenService) CreateAccessToken(oauth2Details *model.OAuth2Details) (*model.OAuth2Token, error) {
+// 根据用户信息和客户端生成访问令牌
+func (tokenService *DefaultTokenService) CreateAccessToken(oauth2Details *model.OAuth2Details) (*model.OAuth2Token, error) {
 	panic("implement me")
 }
 
-func (DefaultTokenService) RefreshAccessToken(refreshTokenValue string) (*model.OAuth2Token, error) {
+// 根据刷新令牌获取访问令牌
+func (tokenService *DefaultTokenService) RefreshAccessToken(refreshTokenValue string) (*model.OAuth2Token, error) {
 	panic("implement me")
 }
 
-func (DefaultTokenService) GetAccessToken(details *model.OAuth2Details) (*model.OAuth2Token, error) {
+// 根据用户信息和客户端信息获取已生成访问令牌
+func (tokenService *DefaultTokenService) GetAccessToken(details *model.OAuth2Details) (*model.OAuth2Token, error) {
 	panic("implement me")
 }
 
-func (DefaultTokenService) ReadAccessToken(tokenValue string) (*model.OAuth2Token, error) {
+// 根据访问令牌获取访问令牌结构体
+func (tokenService *DefaultTokenService) ReadAccessToken(tokenValue string) (*model.OAuth2Token, error) {
 	panic("implement me")
 }
 
